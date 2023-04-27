b5e922d8a5798cb1b03d7ff05c523a468383e079
Souley692 <thirardsoulayman692@gmail.com>
2023-04-27 10:07:29 +0200

    saccades du jugement

---
+++ Projet_Basket_l'ORT/src/Accueil/Accueil.php
@@ -0,0 +1,18 @@
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="Accueil.css">
+    <title>Basket'ORT</title>
+</head>
+<body>
+    <h1>BASKET'ORT</h1> <p>testo</p>
+    <div class="line-1">
+
+    </div>
+
+    <p class="p1">
+TEST
+    </p>
+
+</body>
+</html>
