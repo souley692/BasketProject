3a889220477e796c066b1b2e2effa4487f165826
anthony <a.fromage@lyon.ort.asso.fr>
2023-04-27 09:09:24 +0200

    bo compet ^^

--- Projet_Basket_l'ORT/src/Model/BO/Competition.php
+++ Projet_Basket_l'ORT/src/Model/BO/Competition.php
@@ -5,6 +5,44 @@ namespace src\Model\BO;
 class Competition
 {
   private ?int $idcomp;
-  private ?
+  private ?\DateTime $dat_debComp;
+  private ?\DateTime $dat_finComp;
+  private? int $nb_match_maxComp;
+    function getIdcomp():? int {
+        return $this ->idcomp;
+    }
+    function getDat_debComp():?\DateTime {
+        return $this -> dat_debComp;
+    }
+    function getDat_finComp():? \DateTime{
+        return $this -> dat_finComp;
+    }
+
+    function getNb_match_maxComp():? int {
+    return $this -> nb_match_maxComp;
+    }
+
+    function setIdcomp(int $idComp): void {
+        $this->idcomp = $idComp;
+    }
+    function setDat_debComp(\DateTime $dat_debComp): void {
+        $this->dat_debComp =$dat_debComp;
+    }
+
+    function setDat_finComp(\DateTime $dat_finComp): void {
+        $this->dat_finComp = $dat_finComp;
+    }
+    function setNb_match_maxComp(int $nb_match_maxComp): void {
+    $this->nb_match_maxComp = $nb_match_maxComp;}
+
+    function __construct(?array $datas = null) {
+        if(!is_null($datas)){
+            (isset($datas['numAct'])) ? $this->setNumAct($datas['numAct']) : $this->numAct = null;
+            (isset($datas['nomAct'])) ? $this->setNomAct($datas['nomAct']) : $this->nomAct = '';
+            (isset($datas['preAct'])) ? $this->setPreAct($datas['preAct']) : $this->preAct = '';
+        }
+
+    }
+
 
 }
