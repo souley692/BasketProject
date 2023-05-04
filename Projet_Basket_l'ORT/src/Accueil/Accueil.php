18d589125c708437cae2fd0181ea4e62f33403e4
Souley692 <thirardsoulayman692@gmail.com>
2023-05-04 11:19:04 +0200

    je commit les gars

--- Projet_Basket_l'ORT/src/Accueil/Accueil.php
+++ Projet_Basket_l'ORT/src/Accueil/Accueil.php
@@ -5,15 +5,15 @@
     <title>Basket'ORT</title>
 </head>
 <body>
-    <h1>BASKET'ORT</h1>
+    <h1 style="font-style: italic">BASKET' <img src="../../res/ballon_ort.png" alt="ballon" width="50" height="40">RT</h1>
 
     <div class="line-1"></div> <br>
 
-    <a class="font-pp" href="../Competition/Competition.php">Compétition</a>
-    <a href="../Club/Club.php">Club</a>
-    <a href="../Joueur/Joueur.php">Joueur</a>
-    <a href="../Match/Match.php">Match</a>
-    <a href="../Profil/Profil.php">Profil</a>
+        <a class="font-style_ecrans" href = ../Competition/Competition.php">Competition</a>
+        <a href="../Club/Club.php">Club</a>
+        <a href="../Joueur/Joueur.php">Joueur</a>
+        <a href="../Match/Match.php">Match</a>
+        <a href="../Profil/Profil.php">Profil</a>
 
     <p class="p1">
         TEST
