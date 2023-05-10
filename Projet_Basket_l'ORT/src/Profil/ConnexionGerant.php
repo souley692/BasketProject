25326fdb9e43c7ed9f9d519ba346c4b3d3371237
Branis <branis.kaci@lamache.org>
2023-05-11 01:30:31 +0200

    conexion

--- Projet_Basket_l'ORT/src/Profil/ConnexionGerant.php
+++ Projet_Basket_l'ORT/src/Profil/ConnexionGerant.php
@@ -9,14 +9,14 @@
 
 <body id="formulaire">
     <form action="traitAdmin.php" method="post">
-        <h1 style="font-style: italic"> Bienvenu sur BASKET' <img src="../../res/ballon_ort.png" alt="ballon" width="50" height="40">RT : Admin</h1>
+        <h1 style="font-style: italic"> Bienvenu sur BASKET' <img src="../../res/ballon_ort.png" alt="ballon" width="50" height="40">RT : Gerant</h1>
        <div class="input">
-        <label for="logAdmin"></label>
-        <input id="logAdmin" type="text" name="logAdmin"><br>
-        <label for="mdpAdmin"></label>
-        <input id="mdpAdmin" type="password" name="mdpAdmin"><br>
+        <label for="logGerant"></label>
+        <input id="logGerant" type="text" name="logGerant"><br>
+        <label for="mdpGerant"></label>
+        <input id="mdpGerant" type="password" name="mdpGerant"><br>
     </div>
-        <p class="inscr">Je n'ai pas de <span>compte</span>.Je m'en <span>crée</span> un</p>
+        <p class="inscr">Je n'ai pas de <span>compte</span>.Je demande administratin de m'en <span>crée</span> un</p>
         <div align="center">
             <input type="submit" value="Connexion" id="Connexion" name="buttom">
         </div>
