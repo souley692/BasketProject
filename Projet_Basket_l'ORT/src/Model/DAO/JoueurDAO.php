6275d21a5d27a89eca4f3306a042b2d4ed71a421
Hieu <tranquanghieustudent@gmail.com>
2023-05-11 00:23:18 +0200

    update2

--- Projet_Basket_l'ORT/src/Model/DAO/JoueurDAO.php
+++ Projet_Basket_l'ORT/src/Model/DAO/JoueurDAO.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace DAO;
-require_once '../../Joueur.php';
+use BO\Joueur;
 class JoueurDAO
 {
     private $db;
--- Projet_Basket_l'ORT/tests/testJoueur.php
+++ Projet_Basket_l'ORT/tests/testJoueur.php
@@ -1,1 +1,18 @@
 <?php
+require_once '../config/appConfig.php';
+require_once '../config/globalConfig.php';
+
+use BO\Joueur;
+
+$testJoueur = new Joueur();
+
+$newJoueur = array(
+    'idJoueur'=> '1',
+    'nomJoueur'=>'Bach',
+    'preJoueur'=>'Louis',
+    'dateJouer'=>'08/09/2000',
+    'clubJoueur'=> 'Chicago Bulls'
+);
+
+$testJoueur = new Joueur($newJoueur);
+var_dump($testJoueur);
